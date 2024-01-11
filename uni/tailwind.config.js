e418bfc2231e8c58210f0b0d277b00f7855b7130
Nikita Zytsev <eronmcgonagal@gmail.com>
2024-01-11 20:49:24 +0300

    start project

---
+++ uni/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["./index.html", "./src/**/*.{js,jsx}"];
+export const mode = "jit";
+export const theme = {
+    extend: {
+        colors: {
+            primary: '#7B61FF',
+            primaryLow: '#A9A0F3',
+            back: '#ECF2F9',
+        },
+        fontFamily: {
+            nunito: ["Nunito", "sans-serif"],
+        },
+    },
+    screens: {
+        xs: "480px",
+        ss: "620px",
+        sm: "768px",
+        md: "1060px",
+        lg: "1200px",
+        xl: "1700px",
+    },
+};
+export const plugins = [];
