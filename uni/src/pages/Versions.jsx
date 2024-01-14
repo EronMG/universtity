7ae56e86ce04480e4f04c35fd23b23eea748d5c0
Nikita Zytsev <eronmcgonagal@gmail.com>
2024-01-14 15:20:17 +0300

    commit

--- uni/src/pages/Versions.jsx
+++ uni/src/pages/Versions.jsx
@@ -1,13 +1,14 @@
 import { useCallback, useState } from "react";
-import Caffedra from "../components/Caffedra";
 import "../index.css";
 import styles from "../styles";
 import { BsFiletypeXls } from "react-icons/bs";
 import { FiPlus } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import { useDepartment } from "../context/DepartmentContext";
 
 const Versions = () => {
   const [activeMain, setActiveMain] = useState({});
-
+  const { selectedCaffedra } = useDepartment();
   const handleActiveMain = useCallback(
     (id) => setActiveMain((prevId) => (prevId === id ? null : id)),
     [setActiveMain]
@@ -19,6 +20,7 @@ const Versions = () => {
       name: "Версия 1.0.0",
       date: "2023.09.10",
       year: "2023",
+      redact: <FiPlus />,
       icon: <FiPlus />,
       comments: "...",
     },
@@ -27,6 +29,7 @@ const Versions = () => {
       name: "Версия 2.0.0",
       date: "2023.10.10",
       year: "2023",
+      redact: <FiPlus />,
       icon: <FiPlus />,
       comments: "",
     },
@@ -35,6 +38,7 @@ const Versions = () => {
       name: "Версия 1.0.0",
       date: "2023.10.10",
       year: "2023",
+      redact: <FiPlus />,
       icon: <FiPlus />,
       comments: "Закончить за...",
     },
@@ -52,6 +56,10 @@ const Versions = () => {
     {
       id: "3",
       title: "Год",
+    },
+    {
+      id: "6",
+      title: "Возможность редактирования",
     },
     {
       id: "4",
@@ -86,7 +94,11 @@ const Versions = () => {
       <div>
         <div className="flex flex-col gap-[11px] px-[15px] pt-[11px]">
           <div className="max-w-[289px]">
-            <Caffedra />
+            <div
+              className={`shadowWhite w-[289px] h-[57px] flex items-center justify-center ${styles.text}`}
+            >
+              {selectedCaffedra}
+            </div>
           </div>
           <div className="flex items-center gap-[21px]">
             {arrButton.map((item) => (
@@ -96,36 +108,45 @@ const Versions = () => {
                   styles.textSecond
                 } text-xl shadowSecond text-center max-w-[230px] h-[55px] ${getButtonStyle(
                   item.id
-                )}`}
+                )} active:scale-95 duration-300`}
               >
                 {item.title}
               </button>
             ))}
           </div>
         </div>
-        <div className="border-t-2 mt-[10px]">
+        <div className="border-t-2 mt-[10px] h-[67.6vh]">
           {arrVers.map((item) => (
             <div
               key={item.id}
               onClick={() => handleActiveMain(item.id)}
               className={`${
                 activeMain === item.id ? "mainShadow" : ""
-              } border-b-2 flex pl-[51px] pr-[300px] items-center h-[70px] pt-[15px]`}
+              } border-b-2 flex pr-[50px] items-center h-[70px]  gap-[21px]`}
             >
-              <span className={`${styles.textSecond} text-[24px]`}>
+              <span
+                className={`${styles.textSecond} text-[24px] w-[213px] text-center`}
+              >
                 {item.name}
               </span>
-              <span className={`${styles.textSecond} text-[24px] pl-[105px]`}>
+              <span
+                className={`${styles.textSecond} text-[24px] w-[230px] text-center`}
+              >
                 {item.date}
               </span>
-              <span className={`${styles.textSecond} text-[24px] pl-[92px]`}>
+              <span
+                className={`${styles.textSecond} text-[24px] w-[98px] text-center`}
+              >
                 {item.year}
               </span>
-              <span className="text-[#7B61FF] text-[34px]  pl-[147px]">
+              <span className="text-[#7B61FF] text-[34px]  w-[236px] text-center flex justify-center">
+                {item.redact}
+              </span>
+              <span className="text-[#7B61FF] text-[34px]  w-[236px] text-center flex justify-center">
                 {item.icon}
               </span>
               <span
-                className={`${styles.textSecond} text-[24px] w-[330px] text-center  pl-[107px]`}
+                className={`${styles.textSecond} text-[24px] w-[200px] text-center `}
               >
                 {item.comments}
               </span>
@@ -133,21 +154,30 @@ const Versions = () => {
           ))}
         </div>
       </div>
-      <div className="flex items-center gap-[32px] justify-end pr-[20px] pb-[31px] pt-[47px]">
-        <button
-          className="white text-[#74719E] text-[20px] font-nuni font-[700] flex items-center 
-          gap-[18px] rounded-[32px] bg-white h-[55px] w-[257px] justify-start px-[20px]"
+      <div className="flex items-center justify-between pr-[20px] pb-[31px] pt-[47px]">
+        <Link
+          to="/"
+          className="ml-[30px] white text-[#74719E] text-[20px] font-nuni font-[700] flex items-center 
+          rounded-[32px] bg-white h-[55px] w-[146px]  px-[20px] justify-center active:scale-95 duration-300"
         >
-          <BsFiletypeXls />
-          Экспорт в xls
-        </button>
-        <button
-          className="white flex items-center gap-[5px] text-[#7B61FF] text-[20px] font-nuni 
-          font-[700] rounded-[32px] bg-white h-[55px] w-[246px] justify-start px-[20px]"
-        >
-          <FiPlus />
-          Добавить версию
-        </button>
+          Назад
+        </Link>
+        <div className="flex items-center  gap-[32px]">
+          <button
+            className="white text-[#74719E] text-[20px] font-nuni font-[700] flex items-center 
+          gap-[18px] rounded-[32px] bg-white h-[55px] w-[257px] justify-start px-[20px] active:scale-95 duration-300"
+          >
+            <BsFiletypeXls />
+            Экспорт в xls
+          </button>
+          <button
+            className="white flex items-center gap-[5px] text-[#7B61FF] text-[20px] font-nuni 
+          font-[700] rounded-[32px] bg-white h-[55px] w-[246px] justify-start px-[20px] active:scale-95 duration-300"
+          >
+            <FiPlus />
+            Добавить версию
+          </button>
+        </div>
       </div>
     </div>
   );
