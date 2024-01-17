83304eaf41f2ed56afd679f4f742e6a713e2cbf2
Nikita Zytsev <eronmcgonagal@gmail.com>
2024-01-17 18:06:03 +0300

    feat:func deleting list

--- uni/src/components/ListMain.jsx
+++ uni/src/components/ListMain.jsx
@@ -1,8 +1,13 @@
 import "../index.css";
 import { CiFilter } from "react-icons/ci";
 import { IoIosArrowForward } from "react-icons/io";
+import { useList } from "../context/ListContext";
+import { TfiTrash } from "react-icons/tfi";
+import { FaPencil } from "react-icons/fa6";
 
 const ListMain = () => {
+  const { handleActiveMain, activeMain, handleDelete, lessons } = useList();
+
   const buttons = [
     {
       label: "Факультет",
@@ -36,6 +41,16 @@ const ListMain = () => {
     },
   ];
 
+  const spanWidths = {
+    fac: { width: "100px", "padding-left": "0px" },
+    name: { width: "169px", "padding-left": "19px" },
+    code: { width: "82px", "padding-left": "30px" },
+    title: { width: "61px", "padding-left": "75px" },
+    course: { width: "12px", "padding-left": "134px" },
+    lec: { width: "24px", "padding-left": "97px" },
+    lab: { width: "24px", "padding-left": "120px" },
+  };
+
   function getButtonStyle(id) {
     switch (id) {
       case "1":
@@ -60,7 +75,7 @@ const ListMain = () => {
   return (
     <div className="sd py-[14px] px-[12px] w-full mt-[6px]">
       <div className="shadowSecond h-full">
-        <div className="px-[10px] pt-[13px] flex justify-between">
+        <div className="px-[10px] pt-[13px] flex justify-between pb-[10px]">
           <div className="flex gap-[11px]">
             {buttons.map((item) => (
               <button
@@ -79,7 +94,54 @@ const ListMain = () => {
             <IoIosArrowForward className="text-[22px] rotate-90" />
           </button>
         </div>
-        <div></div>
+        <div className="h-[90%] overflow-scroll">
+          {lessons.length === 0 ? (
+            <div className="shadowWhite text-[#424242] text-[14px] font-nuni font-[700] w-[262px] h-[55px] flex items-center justify-center">
+              Записей нет
+            </div>
+          ) : (
+            <div className="flex flex-col w-full">
+              {lessons.map((item) => (
+                <div
+                  onClick={() => handleActiveMain(item.id)}
+                  key={item.id}
+                  className={`${
+                    activeMain === item.id ? "mainShadow" : ""
+                  } flex h-[70px] border-t-2 pl-[20px] w-full`}
+                >
+                  <div className="w-[852px] flex items-center">
+                    {Object.keys(item).map(
+                      (key) =>
+                        key !== "id" && (
+                          <span
+                            style={spanWidths[key]}
+                            key={key}
+                            className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center"
+                          >
+                            {item[key]}
+                          </span>
+                        )
+                    )}
+                  </div>
+                  {activeMain === item.id && (
+                    <div className="absolute right-0 -top-12 p-2 flex gap-6">
+                      <button className="list w-[160px] h-[33px] justify-center text-primary text-[20px] font-nuni font-[700] flex items-center gap-2">
+                        Изменить
+                        <FaPencil className="text-black text-[16px]" />
+                      </button>
+                      <button
+                        onClick={() => handleDelete(item.id)}
+                        className="list w-[160px] h-[33px] justify-center text-primary text-[20px] font-nuni font-[700] flex items-center gap-2"
+                      >
+                        Удалить <TfiTrash className="text-black text-[16px]" />
+                      </button>
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
--- uni/src/context/ListContext.jsx
+++ uni/src/context/ListContext.jsx
@@ -7,6 +7,160 @@ const ListContext = createContext();
 const ListProvider = ({ children }) => {
   const [zap, setZap] = useState(false);
   const [up, setUp] = useState(false);
+  const [activeMain, setActiveMain] = useState({});
+  const [lessons, setLessons] = useState([
+    {
+      id: "1",
+      fac: "ФКТиПМ",
+      name: "Алгебра",
+      code: "02.03.02",
+      title: "ФИИТ",
+      course: "1",
+      lec: "40",
+      lab: "20",
+    },
+    {
+      id: "2",
+      fac: "ФКТиПМ",
+      name: "АиПиС",
+      code: "02.03.02",
+      title: "ФИИТ",
+      course: "3",
+      lec: "10",
+      lab: "10",
+    },
+    {
+      id: "3",
+      fac: "ФКТиПМ",
+      name: "Математическое моделирование",
+      code: "02.03.02",
+      title: "ФИИТ",
+      course: "2",
+      lec: "20",
+      lab: "30",
+    },
+    {
+      id: "4",
+      fac: "ФКТиПМ",
+      name: "Алгебра",
+      code: "02.03.02",
+      title: "ФИИТ",
+      course: "1",
+      lec: "40",
+      lab: "20",
+    },
+    {
+      id: "5",
+      fac: "ФКТиПМ",
+      name: "АиПиС",
+      code: "02.03.02",
+      title: "ФИИТ",
+      course: "3",
+      lec: "10",
+      lab: "10",
+    },
+    {
+      id: "6",
+      fac: "ФКТиПМ",
+      name: "Математическое моделирование",
+      code: "02.03.02",
+      title: "ФИИТ",
+      course: "2",
+      lec: "20",
+      lab: "30",
+    },
+    {
+      id: "7",
+      fac: "ФКТиПМ",
+      name: "Алгебра",
+      code: "02.03.02",
+      title: "ФИИТ",
+      course: "1",
+      lec: "40",
+      lab: "20",
+    },
+    {
+      id: "8",
+      fac: "ФКТиПМ",
+      name: "АиПиС",
+      code: "02.03.02",
+      title: "ФИИТ",
+      course: "3",
+      lec: "10",
+      lab: "10",
+    },
+    {
+      id: "9",
+      fac: "ФКТиПМ",
+      name: "Математическое моделирование",
+      code: "02.03.02",
+      title: "ФИИТ",
+      course: "2",
+      lec: "20",
+      lab: "30",
+    },
+    {
+      id: "10",
+      fac: "ФКТиПМ",
+      name: "Алгебра",
+      code: "02.03.02",
+      title: "ФИИТ",
+      course: "1",
+      lec: "40",
+      lab: "20",
+    },
+    {
+      id: "11",
+      fac: "ФКТиПМ",
+      name: "АиПиС",
+      code: "02.03.02",
+      title: "ФИИТ",
+      course: "3",
+      lec: "10",
+      lab: "10",
+    },
+    {
+      id: "12",
+      fac: "ФКТиПМ",
+      name: "Математическое моделирование",
+      code: "02.03.02",
+      title: "ФИИТ",
+      course: "2",
+      lec: "20",
+      lab: "30",
+    },
+    {
+      id: "13",
+      fac: "ФКТиПМ",
+      name: "Алгебра",
+      code: "02.03.02",
+      title: "ФИИТ",
+      course: "1",
+      lec: "40",
+      lab: "20",
+    },
+    {
+      id: "14",
+      fac: "ФКТиПМ",
+      name: "АиПиС",
+      code: "02.03.02",
+      title: "ФИИТ",
+      course: "3",
+      lec: "10",
+      lab: "10",
+    },
+    {
+      id: "15",
+      fac: "ФКТиПМ",
+      name: "Математическое моделирование",
+      code: "02.03.02",
+      title: "ФИИТ",
+      course: "2",
+      lec: "20",
+      lab: "30",
+    },
+  ]);
+
   //! Cоздать запись Функицонал
 
   const handleZap = useCallback(() => {
@@ -24,8 +178,38 @@ const ListProvider = ({ children }) => {
   }, []);
 
   // ! Загруженные из УП Функционал
+
+  // TODO: ADD FUNCTIONAL FOR MAINSCREEN
+
+  const handleActiveMain = useCallback(
+    (id) => setActiveMain((prevId) => (prevId === id ? null : id)),
+    [setActiveMain]
+  );
+
+  // TODO: ADD FUNCTIONAL FOR MAINSCREEN
+
+  //! DELETE
+
+  const handleDelete = (id) => {
+    const updatedLessons = lessons.filter((item) => item.id !== id);
+    setLessons(updatedLessons);
+    setActiveMain(null); // Clear activeMain after deletion if needed
+  };
+
+  //! DELETE
   return (
-    <ListContext.Provider value={{ handleZap, zap, handleUp, up }}>
+    <ListContext.Provider
+      value={{
+        handleZap,
+        zap,
+        handleUp,
+        up,
+        handleActiveMain,
+        activeMain,
+        handleDelete,
+        lessons,
+      }}
+    >
       {children}
     </ListContext.Provider>
   );
