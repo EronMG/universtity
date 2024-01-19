669099c1adefa941115f1647f3d96c71429b02a5
Nikita Zytsev <eronmcgonagal@gmail.com>
2024-01-19 15:38:47 +0300

    fix: change DirectionForm

--- uni/src/components/DirectionForm.jsx
+++ uni/src/components/DirectionForm.jsx
@@ -4,6 +4,7 @@ import Modal from "react-modal";
 import { useDepartment } from "../context/DepartmentContext";
 import styles from "../styles";
 import "../index.css";
+import { IoCloseOutline } from "react-icons/io5";
 
 // eslint-disable-next-line react/prop-types
 const DirectionForm = ({ isOpen, onRequestClose, onDirectionAdded }) => {
@@ -11,17 +12,46 @@ const DirectionForm = ({ isOpen, onRequestClose, onDirectionAdded }) => {
   const [date, setDate] = useState("");
   const [name, setName] = useState("");
   const [active, setActive] = useState(false);
+  const [fullName, setFullName] = useState("");
+  const [selectedFaculty, setSelectedFaculty] = useState(null);
 
   const handleActive = useCallback(() => setActive((prev) => !prev), []);
 
   const handleSubmit = () => {
-    if (date && name) {
-      onDirectionAdded({ id: Math.random().toString(), date, name });
+    if (date && name && fullName) {
+      onDirectionAdded({
+        id: Math.random().toString(),
+        date,
+        name,
+        fullName,
+      });
+      setDate("");
+      setName("");
+      setFullName("");
+      setSelectedFaculty(null);
       onRequestClose();
     } else {
-      alert("Please enter both date and name for the new direction.");
+      alert("Please enter date, name, and full name for the new direction.");
     }
   };
+
+  const handleCancel = () => {
+    setDate("");
+    setName("");
+    setFullName("");
+    setSelectedFaculty(null);
+  };
+
+  const handleCancel1 = () => {
+    onRequestClose();
+  };
+
+  const handleActive1 = (faculty) => {
+    setSelectedFaculty(faculty);
+    setActive((prev) => !prev);
+  };
+
+  const isSaveDisabled = !date || !name || !fullName;
 
   return (
     <Modal
@@ -32,10 +62,17 @@ const DirectionForm = ({ isOpen, onRequestClose, onDirectionAdded }) => {
       overlayClassName="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center"
     >
       <div className="shadowWhite w-[709px] py-3">
-        <h2 className="text-black text-2xl font-nuni font-bold px-8">
-          Добавление направления
-        </h2>
-        <div className="px-4">
+        <div className="flex justify-between px-8 items-center">
+          {" "}
+          <h2 className="text-black text-2xl font-nuni font-bold">
+            Добавление направления
+          </h2>
+          <IoCloseOutline
+            onClick={handleCancel1}
+            className="text-primary text-4xl cursor-pointer"
+          />
+        </div>
+        <div className="px-4 flex flex-col gap-7 pt-12">
           <div className="modaldiv flex h-12 overflow-hidden items-center border-[1px] border-[#B8CCE0]">
             <label className="px-2 text-[#6C6993] text-2xl font-nuni font-bold border-r-2 h-full flex items-center w-[235px] justify-center">
               Код направления
@@ -46,48 +83,90 @@ const DirectionForm = ({ isOpen, onRequestClose, onDirectionAdded }) => {
               onChange={(e) => {
                 const inputValue = e.target.value.replace(/[^0-9.]/g, ""); // Allow only numbers and dots
                 const formattedValue = inputValue
-                  .split(".") // Split the input by dots
-                  .map((part) => (part.length > 2 ? part.slice(0, 2) : part)) // Limit each part to 2 characters
-                  .join(".") // Join the parts with dots
-                  .slice(0, 8); // Limit the entire input to 8 characters
+                  .split(".")
+                  .map((part) => (part.length > 2 ? part.slice(0, 2) : part))
+                  .join(".")
+                  .slice(0, 8);
                 setDate(formattedValue);
               }}
-              className="bg-transparent h-full outline-none text-black text-2xl font-nuni font-bold text-center"
+              className="bg-transparent h-full outline-none text-black text-2xl font-nuni font-bold text-center w-[435px]"
             />
           </div>
           <div className="modaldiv flex h-12 overflow-hidden items-center border-[1px] border-[#B8CCE0]">
             <label className="px-2 text-[#6C6993] text-2xl font-nuni font-bold border-r-2 h-full flex items-center w-[235px] justify-center">
               Факультет
             </label>
-            <div className="w-[440px] h-full">
+            <div className=" w-[440px] h-full">
               <span
                 onClick={handleActive}
-                className="h-full shadowWhite text-black text-2xl font-nuni font-bold w-full flex items-center justify-center z-20"
+                className="relative z-10 h-full shadowWhite text-black text-2xl font-nuni font-bold w-full flex items-center justify-center cursor-pointer"
               >
-                Выбрать факультет
+                {selectedFaculty
+                  ? ` ${selectedFaculty.name}`
+                  : "Выбрать факультет"}
               </span>
               {active && (
-                <div className="absolute">
-                  {arrCaf.map((item) => (
-                    <div
-                      key={item.id}
-                      className={`side2 flex items-center justify-center gap-4 h-14 ${styles.text}`}
-                    >
-                      {item.date} {item.name}
-                    </div>
-                  ))}
+                <div className="shadowThird px-4 pt-10 pb-5 w-[440px] absolute top-[185px]">
+                  <div>
+                    {arrCaf.map((item) => (
+                      <div
+                        key={item.id}
+                        onClick={() => handleActive1(item)}
+                        className={`side2 flex items-center justify-center gap-4 h-14 ${styles.text} cursor-pointer`}
+                      >
+                        {item.date} {item.name}
+                      </div>
+                    ))}
+                  </div>
                 </div>
               )}
             </div>
           </div>
         </div>
-        <div></div>
-        <button onClick={handleSubmit}>Save Direction</button>
+        <div className="px-4 flex flex-col gap-7 pt-12">
+          <div className="modaldiv flex h-12 overflow-hidden items-center border-[1px] border-[#B8CCE0]">
+            <label className="px-2 text-[#6C6993] text-2xl font-nuni font-bold border-r-2 h-full flex items-center w-[235px] justify-center">
+              Название
+            </label>
+            <input
+              type="text"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+              className="bg-transparent h-full outline-none text-black text-2xl font-nuni font-bold w-[435px]"
+            />
+          </div>
+          <div className="modaldiv flex h-12 overflow-hidden items-center border-[1px] border-[#B8CCE0]">
+            <label className="px-2 text-[#6C6993] text-xl font-nuni font-bold border-r-2 h-full flex items-center w-[235px] justify-center">
+              Короткое название
+            </label>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="bg-transparent h-full outline-none text-black text-2xl font-nuni font-bold text-center w-[435px]"
+            />
+          </div>
+        </div>
+        <div className="flex justify-between items-center px-4 pt-20">
+          <button
+            onClick={handleSubmit}
+            disabled={isSaveDisabled}
+            className={`${
+              isSaveDisabled ? "opacity-50 cursor-not-allowed" : "shadowSecond"
+            } w-72 flex items-center justify-center text-[#74719E] h-14 text-xl font-nuni font-bold`}
+          >
+            Сохранить направление
+          </button>
+          <button
+            onClick={handleCancel}
+            className="shadowSecond w-72 flex items-center justify-center text-[#74719E] h-14 text-xl font-nuni font-bold"
+          >
+            Отмена
+          </button>
+        </div>
       </div>
     </Modal>
   );
 };
 
 export default DirectionForm;
-
-// <input type="text" value={name} onChange={(e) => setName(e.target.value)} />;
