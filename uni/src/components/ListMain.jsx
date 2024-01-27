13217f18fe116677eeb0ae6f373207a8ab8cf163
Nikita Zytsev <eronmcgonagal@gmail.com>
2024-01-27 19:30:20 +0300

    commit

--- uni/src/components/ListMain.jsx
+++ uni/src/components/ListMain.jsx
@@ -50,6 +50,45 @@ const ListMain = () => {
     },
   ];
 
+  const buttonsSec = [
+    {
+      label: "Курс",
+      id: "1",
+    },
+    {
+      label: "Всего студентов",
+      id: "2",
+    },
+    {
+      label: "Бюджет",
+      id: "3",
+    },
+    {
+      label: "Договор",
+      id: "4",
+    },
+    {
+      label: "Кол-во лек. потоков",
+      id: "5",
+    },
+    {
+      label: "Кол-во групп",
+      id: "6",
+    },
+    {
+      label: "Кол-во подгрупп",
+      id: "7",
+    },
+    {
+      label: "Кол-во профилей",
+      id: "8",
+    },
+    {
+      label: "Кол-во групп для практики",
+      id: "9",
+    },
+  ];
+
   function getButtonStyle(id) {
     switch (id) {
       case "1":
@@ -76,7 +115,30 @@ const ListMain = () => {
         return ""; // Возвращайте пустую строку или другой класс по умолчанию
     }
   }
-
+  function getButtonStyle2(id) {
+    switch (id) {
+      case "1":
+        return "w-[95px]"; // Замените "buttonStyle1" на ваш класс стиля для кнопки с id "1"
+      case "2":
+        return "w-[182px]";
+      case "3":
+        return "w-[110px]";
+      case "4":
+        return "w-[110px]";
+      case "5":
+        return "w-[150px]";
+      case "6":
+        return "w-[104px]";
+      case "7":
+        return "w-[129px]";
+      case "8":
+        return "w-[124px]";
+      case "9":
+        return "w-[177px]";
+      default:
+        return ""; // Возвращайте пустую строку или другой класс по умолчанию
+    }
+  }
   return (
     <div className="sd py-[14px] px-[12px] w-full mt-[6px]">
       <div className="shadowSecond h-full">
@@ -94,17 +156,30 @@ const ListMain = () => {
         )}
         <div className="px-[10px] pt-[13px] flex justify-between pb-[10px] ">
           <div className="flex gap-[15px]">
-            {buttons.map((item) => (
-              <button
-                className={`shadowSecond flex flex-col items-center justify-center text-primary text-lg font-nuni font-[700] h-[55px]  ${getButtonStyle(
-                  item.id
-                )}`}
-                key={item.id}
-              >
-                {item.label}
-                <span className="text-[#424242] text-[15px]">{item.title}</span>
-              </button>
-            ))}
+            {item !== "2"
+              ? buttons.map((item) => (
+                  <button
+                    className={`shadowSecond flex flex-col items-center justify-center text-primary text-lg font-nuni font-[700] h-[55px]  ${getButtonStyle(
+                      item.id
+                    )}`}
+                    key={item.id}
+                  >
+                    {item.label}
+                    <span className="text-[#424242] text-[15px]">
+                      {item.title}
+                    </span>
+                  </button>
+                ))
+              : buttonsSec.map((item) => (
+                  <button
+                    className={`shadowSecond flex flex-col items-center justify-center text-primary text-lg font-nuni font-[700] h-[55px]  ${getButtonStyle2(
+                      item.id
+                    )}`}
+                    key={item.id}
+                  >
+                    {item.label}
+                  </button>
+                ))}
           </div>
           <ListFilter />
         </div>
--- uni/src/components/ListSubject.jsx
+++ uni/src/components/ListSubject.jsx
@@ -4,6 +4,7 @@ import { FaPencil } from "react-icons/fa6";
 import { useDepartment } from "../context/DepartmentContext";
 import { IoIosArrowDown } from "react-icons/io";
 import ContForm from "./ContForm";
+import { useState } from "react";
 const ListSubject = () => {
   const { handleToggleSideBar, isSideBarVisible } = useDepartment();
   const {
@@ -16,9 +17,54 @@ const ListSubject = () => {
     isContForm,
   } = useList();
 
+  const [pride, setPride] = useState([
+    {
+      id: "1",
+      name: "1",
+      students: "96",
+      mon: "40",
+      dog: "40",
+      potoc: "1",
+      group: "4",
+      pod: "8",
+      prof: "3",
+      prac: "4",
+    },
+    {
+      id: "2",
+      name: "2",
+      students: "56",
+      mon: "40",
+      dog: "10",
+      potoc: "1",
+      group: "4",
+      pod: "8",
+      prof: "3",
+      prac: "4",
+    },
+    {
+      id: "3",
+      name: "3",
+      students: "73",
+      mon: "40",
+      dog: "20",
+      potoc: "1",
+      group: "4",
+      pod: "8",
+      prof: "3",
+      prac: "4",
+    },
+  ]);
+
+  const handlePride = (id) => {
+    const updatedLessons = pride.filter((item) => item.id !== id);
+    setPride(updatedLessons);
+  };
+
   const handleButtonClick = () => {
     setIsContForm(true);
   };
+
   return (
     <div
       className={`h-[90%] ${
@@ -41,88 +87,144 @@ const ListSubject = () => {
           } text-black text-[36px]`}
         />
       </div>
-      {lessons.length === 0 ? (
+      {item !== "2" && lessons.length === 0 ? (
         <div className="shadowWhite text-[#424242] text-[14px] font-nuni font-[700] w-[262px] h-[55px] flex items-center justify-center">
           Записей нет
         </div>
       ) : (
-        <div className="flex flex-col w-full">
-          {lessons.map((boot) => (
-            <div
-              onClick={() => handleActiveMain(boot.id)}
-              key={boot.id}
-              className={`${
-                activeMain === boot.id ? "mainShadow" : ""
-              } flex h-[70px] border-t-2 px-[20px] w-full`}
-            >
-              <div className=" flex items-center">
-                <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center w-24">
-                  ФКТиПМ
-                </span>
-                <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center w-48 pl-6">
-                  {boot.title}
-                </span>
-                <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center w-20 pl-5">
-                  {boot.direction.date}
-                </span>
-                <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center w-16 pl-[85px]">
-                  {boot.direction.name}
-                </span>
-                <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center w-3 pl-32">
-                  {boot.course}
-                </span>
-                <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center w-6 pl-28">
-                  {boot.lectures}
-                </span>
-                <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center w-6 pl-32">
-                  {boot.labs}
-                </span>
-                <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center w-6 pl-[130px]">
-                  {boot.labs}
-                </span>
-                <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center w-6 pl-[130px]">
-                  {boot.practice}
-                </span>
-                <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center w-6 pl-[115px]">
-                  {boot.caf}/{boot.rucfac}
-                </span>
-              </div>
-              {activeMain === boot.id && item !== "3" ? (
-                <div className={`absolute  flex gap-6 right-0 top-16 p-2`}>
-                  <button className="list w-[160px] h-[33px] justify-center text-primary text-[20px] font-nuni font-[700] flex items-center gap-2">
-                    Изменить
-                    <FaPencil className="text-black text-[16px]" />
-                  </button>
-                  <button
-                    onClick={() => handleDelete(boot.id)}
-                    className="list w-[160px] h-[33px] justify-center text-primary text-[20px] font-nuni font-[700] flex items-center gap-2"
-                  >
-                    Удалить <TfiTrash className="text-black text-[16px]" />
-                  </button>
+        <div className="flex flex-col w-full h-full">
+          {item !== "2"
+            ? lessons.map((boot) => (
+                <div
+                  onClick={() => handleActiveMain(boot.id)}
+                  key={boot.id}
+                  className={`${
+                    activeMain === boot.id ? "mainShadow" : ""
+                  } flex h-[70px] border-t-2 px-[20px] w-full`}
+                >
+                  <div className=" flex items-center">
+                    <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center w-24">
+                      ФКТиПМ
+                    </span>
+                    <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center w-48 pl-6">
+                      {boot.title}
+                    </span>
+                    <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center w-20 pl-5">
+                      {boot.direction.date}
+                    </span>
+                    <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center w-16 pl-[85px]">
+                      {boot.direction.name}
+                    </span>
+                    <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center w-3 pl-32">
+                      {boot.course}
+                    </span>
+                    <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center w-6 pl-28">
+                      {boot.lectures}
+                    </span>
+                    <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center w-6 pl-32">
+                      {boot.labs}
+                    </span>
+                    <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center w-6 pl-[130px]">
+                      {boot.labs}
+                    </span>
+                    <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center w-6 pl-[130px]">
+                      {boot.practice}
+                    </span>
+                    <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center w-6 pl-[115px]">
+                      {boot.caf}/{boot.rucfac}
+                    </span>
+                  </div>
+                  {activeMain === boot.id && item !== "3" ? (
+                    <div className={`absolute  flex gap-6 right-0 top-16 p-2`}>
+                      <button className="list w-[160px] h-[33px] justify-center text-primary text-[20px] font-nuni font-[700] flex items-center gap-2">
+                        Изменить
+                        <FaPencil className="text-black text-[16px]" />
+                      </button>
+                      <button
+                        onClick={() => handleDelete(boot.id)}
+                        className="list w-[160px] h-[33px] justify-center text-primary text-[20px] font-nuni font-[700] flex items-center gap-2"
+                      >
+                        Удалить <TfiTrash className="text-black text-[16px]" />
+                      </button>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                  {activeMain === boot.id && item === "3" ? (
+                    <div className={`absolute  flex gap-6 right-6 top-4 p-2`}>
+                      <button
+                        onClick={() => handleButtonClick()} // Add parentheses to invoke the function
+                        className="justify-center text-primary text-[20px] font-nuni font-[700] flex items-center gap-2"
+                      >
+                        <FaPencil className=" text-[16px] text-primary" />
+                      </button>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                  {isContForm && (
+                    <ContForm
+                      isOpen={isContForm}
+                      onRequestClose={() => setIsContForm(false)}
+                    />
+                  )}
                 </div>
-              ) : (
-                ""
-              )}
-              {activeMain === boot.id && item === "3" ? (
-                <div className={`absolute  flex gap-6 right-6 top-4 p-2`}>
-                  <button
-                    onClick={() => handleButtonClick()} // Add parentheses to invoke the function
-                    className="justify-center text-primary text-[20px] font-nuni font-[700] flex items-center gap-2"
-                  >
-                    <FaPencil className=" text-[16px] text-primary" />
-                  </button>
+              ))
+            : pride.map((boot) => (
+                <div
+                  onClick={() => handleActiveMain(boot.id)}
+                  key={boot.id}
+                  className={`${
+                    activeMain === boot.id ? "mainShadow" : ""
+                  } flex h-[70px] border-t-2 px-[8px] w-full`}
+                >
+                  <div className=" flex items-center gap-[15px]">
+                    <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center w-[95px]">
+                      {boot.name}
+                    </span>
+                    <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center  w-[182px]">
+                      {boot.students}
+                    </span>
+                    <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center  w-[110px]">
+                      {boot.mon}
+                    </span>
+                    <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center  w-[110px]">
+                      {boot.dog}
+                    </span>
+                    <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center  w-[150px]">
+                      {boot.potoc}
+                    </span>
+                    <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center w-[104px]">
+                      {boot.group}
+                    </span>
+                    <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center  w-[129px]">
+                      {boot.pod}
+                    </span>
+                    <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center w-[124px]">
+                      {boot.prof}
+                    </span>
+                    <span className="font-nuni font-[700] text-[20px] text-[#424242] h-fit text-center w-[177px]">
+                      {boot.prac}
+                    </span>
+                  </div>
+                  {activeMain === boot.id && item !== "3" ? (
+                    <div className={`absolute  flex gap-6 right-0 top-16 p-2`}>
+                      <button className="list w-[160px] h-[33px] justify-center text-primary text-[20px] font-nuni font-[700] flex items-center gap-2">
+                        Изменить
+                        <FaPencil className="text-black text-[16px]" />
+                      </button>
+                      <button
+                        onClick={() => handlePride(boot.id)}
+                        className="list w-[160px] h-[33px] justify-center text-primary text-[20px] font-nuni font-[700] flex items-center gap-2"
+                      >
+                        Удалить <TfiTrash className="text-black text-[16px]" />
+                      </button>
+                    </div>
+                  ) : (
+                    ""
+                  )}
                 </div>
-              ) : (
-                ""
-              )}
-              {isContForm && (
-                <ContForm
-                  isOpen={isContForm}
-                  onRequestClose={() => setIsContForm(false)}
-                />
-              )}
-            </div>
-          ))}
+              ))}
         </div>
       )}
     </div>
